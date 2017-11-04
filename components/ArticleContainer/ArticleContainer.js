2c242fb1045ec01f5442e71de5efac5152ec0d22
Andres Valdovinos <valdovinosandres@Andress-MacBook-Pro.local>
2017-11-03 21:12:07 -0700

    template

--- components/ArticleContainer/ArticleContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ArticleContainer = props =>
-  <div class="container-fluid article-container">
-  </div>
-
-export default ArticleContainer;
--- server.js
+++ server.js
@@ -1,48 +1,19 @@
-// Web Scraper Homework Solution Example
-// (be sure to watch the video to see
-// how to operate the site in the browser)
-// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Require our dependencies
-var express = require("express");
-var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// Set up our port to be either the host's designated port, or 3000
-var PORT = process.env.PORT || 3000;
-
-// Instantiate our Express App
-var app = express();
-
-// Require our routes
-var routes = require("./routes");
-
-// Designate our public folder as a static directory
-app.use(express.static("public"));
-
-// Connect Handlebars to our Express app
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Use bodyParser in our app
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Have every request go through our route middleware
-app.use(routes);
-
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Listen on the port
 app.listen(PORT, function() {
-  console.log("Listening on port: " + PORT);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
